f45f8bb76eae038dc4acf9c71fcea435e0407f09
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-05-09 18:16:26 +0500

    more comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 #include <wifi.h>
 
-//my wifi name and password
-const char* wifiName="HASEEB";
-const char* wifiPassword="65464816";
+// my wifi name and password
+const char *wifiName = "HASEEB";
+const char *wifiPassword = "65464816";
 WiFiServer server(80);
 
-//these are the GPIO pins of esp32
-const int trigPin = 27;//setting the trig pin
-const int echoPin = 25;//setting the echo pin 
-const int ledPin=18;//setting the ledpin
+// these are the GPIO pins of esp32
+const int trigPin = 27; // setting the trig pin
+const int echoPin = 25; // setting the echo pin
+const int ledPin = 18;  // setting the ledpin
 
-//define sound speed in cm/uS
+// define sound speed in cm/uS
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
 
@@ -19,27 +19,30 @@ long duration;
 float distanceCm;
 float distanceInch;
 
-void setup() {
-  Serial.begin(115200); // Starts the serial communication
+void setup()
+{
+  Serial.begin(115200);     // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  pinMode (ledPin, OUTPUT);//sets the ledPin as another output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  pinMode(ledPin, OUTPUT);  // sets the ledPin as another output
 
   Serial.print("Connecting to ");
   Serial.println(wifiName);
   WiFi.begin(wifiName, wifiPassword);
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print("Trying to connect to Wifi Network");
+  while (WiFi.status() != WL_CONNECTED)//checks if the wifi is connected
+  {
+    delay(1000);
+    Serial.print("Trying to connect to Wifi Network");
   }
   Serial.println("");
   Serial.println("Successfully connected to WiFi network");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+  Serial.println(WiFi.localIP());//prints the Ip adress so we can go there
+  server.begin();//start the server
 }
 
-void loop() {
+void loop()
+{
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -47,29 +50,31 @@ void loop() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-  
+
   // Calculate the distance
-  distanceCm = duration * SOUND_SPEED/2;//s=vt
-  
+  distanceCm = duration * SOUND_SPEED / 2; // s=vt
+
   // Convert to inches
   distanceInch = distanceCm * CM_TO_INCH;
-  
+
   // Prints the distance in the Serial Monitor
   Serial.print("Distance (cm): ");
   Serial.println(distanceCm);
   Serial.print("Distance (inch): ");
   Serial.println(distanceInch);
-  if(distanceCm<=20){//open the door if distance less than 20cm
+  if (distanceCm <= 20)
+  { // open the door if distance less than 20cm
     digitalWrite(ledPin, HIGH);
-      Serial.print("Opening Door!");
+    Serial.print("Opening Door!");
   }
-  else{//close the door otherwise
+  else
+  { // close the door otherwise
     digitalWrite(ledPin, LOW);
     Serial.print("Door Closed!");
   }
-  
-  delay(5000);//5 second delay before loop resarts
+
+  delay(5000); // 5 second delay before loop resarts
 }
