34af2e8a943d51bc1b26b46ab9a1b46a6dfe0964
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-05-28 12:47:06 +0500

    everything works, added wifi reconnection capabilities

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 #include <Adafruit_BME280.h>
 
 // my wifi name and password
-const char *ssid = "HASEEB";
-const char *password = "65464816";
+const char *ssid = "Ali's a52";
+const char *password = "qwerty123";
 
 // Set web server port number to 80
 AsyncWebServer server(80);
@@ -144,38 +144,8 @@ void setup()
 void loop()
 {
 
-  //     // Clears the trigPin
-  // digitalWrite(trigPin, LOW);
-  // delayMicroseconds(2);
-  // // Sets the trigPin on HIGH state for 10 micro seconds
-  // digitalWrite(trigPin, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(trigPin, LOW);
-
-  // // Reads the echoPin, returns the sound wave travel time in microseconds
-  // duration = pulseIn(echoPin, HIGH);
-
-  // // Calculate the distance
-  // distanceCm = duration * SOUND_SPEED / 2; // s=vt
-
-  // // Convert to inches
-  // distanceInch = distanceCm * CM_TO_INCH;
-
-  // // Prints the distance in the Serial Monitor
-  // Serial.print("Distance (cm): ");
-  // Serial.println(distanceCm);
-  // Serial.print("Distance (inch): ");
-  // Serial.println(distanceInch);
-  // if (distanceCm <= 20)
-  // { // open the door if distance less than 20cm
-  //   digitalWrite(ledPin, HIGH);
-  //   Serial.print("Opening Door!");
-  // }
-  // else
-  // { // close the door otherwise
-  //   digitalWrite(ledPin, LOW);
-  //   Serial.print("Door Closed!");
-  // }
-
-  // delay(5000); // 5 second delay before loop resarts
+// if WiFi is down, restart esp32 to reconnect
+  if ((WiFi.status() != WL_CONNECTED)) {
+    ESP.restart();
+  }
 }
